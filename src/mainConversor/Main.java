b0283db3d145757d0ec075c11595bc18593a873e
Robinson Henao Botero <robinson.henao.b@gmail.com>
2023-08-08 01:58:57 -0500

    validacion de respuestas cancel,no,cierre de ventana

--- src/mainConversor/Main.java
+++ src/mainConversor/Main.java
@@ -10,54 +10,67 @@ public class Main {
         conversorDivisas operacionDivisas = new conversorDivisas();
         conversorLongitud operacionLongitud = new conversorLongitud();
 
-        while (true){
-            String conversion = (JOptionPane.showInputDialog(null, "seleccione una opcion de conversion", "Conversion",
-                    JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Conversor de Moneda", "Conversor de Longitud"}, "conversor de moneda")).toString();
-
-            if (conversion.equals("Conversor de Moneda")) {
+        while (true) {
+            Object conversion = (JOptionPane.showInputDialog(null, "seleccione una opcion de conversion", "Conversion",
+                    JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Conversor de Moneda", "Conversor de Longitud"}, "conversor de moneda"));
+            if (conversion == null) {
+                JOptionPane.showMessageDialog(null, "Programa terminado");
+                break;
+            } else if (conversion.toString().equals("Conversor de Moneda")) {
                 String entrada = JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero que desea convertir");
-                if (isNumeric(entrada)) {
+                if (entrada == null) {
+                    JOptionPane.showMessageDialog(null, "Programa terminado");
+                    break;
+                } else if (isNumeric(entrada)) {
                     double valor = Integer.parseInt(entrada);
-
                     //De Pesos a Dolar,Euro,Libras,Yen,Won Coreano
-                    String divisas = (JOptionPane.showInputDialog(null, "seleccione una opcion de conversion", "Divisas",
+                    Object divisas = (JOptionPane.showInputDialog(null, "seleccione una opcion de conversion", "Divisas",
                             JOptionPane.PLAIN_MESSAGE, null, new Object[]{"De Pesos a Dolar", "De Pesos a Euro", "De Pesos a Libras", "De Pesos a Yen",
-                                    "De Pesos a Won Coreano","De Pesos a Rublos", "De Dolar a Pesos", "De Euro a Pesos", "De Libras a Pesos", "De Yen a Pesos",
-                                    "De Won Coreano a Pesos","De Rublos a Pesos"}, "De Pesos a Dolar")).toString();
+                                    "De Pesos a Won Coreano", "De Pesos a Rublos", "De Dolar a Pesos", "De Euro a Pesos", "De Libras a Pesos", "De Yen a Pesos",
+                                    "De Won Coreano a Pesos", "De Rublos a Pesos"}, "De Pesos a Dolar"));
 
-                    JOptionPane.showMessageDialog(null, operacionDivisas.convertirDivisas(valor, divisas));
-
-                    if (JOptionPane.OK_OPTION != JOptionPane.showConfirmDialog(null,"¿Deseas realizar otra conversión?")) {
+                    if (divisas == null) {
                         JOptionPane.showMessageDialog(null, "Programa terminado");
                         break;
+                    } else {
+                        JOptionPane.showMessageDialog(null, operacionDivisas.convertirDivisas(valor, divisas.toString()));
+                        int opc = JOptionPane.showConfirmDialog(null, "¿Deseas realizar otra conversión?");
+                        if (opc != 0) {
+                            JOptionPane.showMessageDialog(null, "Programa terminado");
+                            break;
+                        }
                     }
-
                 } else {
                     JOptionPane.showMessageDialog(null, "Valor inválido");
                 }
             } else {
                 String entrada = JOptionPane.showInputDialog(null, "Ingrese la medida de longitud que desea convertir");
-                if (isNumeric(entrada)) {
+                if (entrada == null) {
+                    JOptionPane.showMessageDialog(null, "Programa terminado");
+                    break;
+                } else if (isNumeric(entrada)) {
                     double medidad = Integer.parseInt(entrada);
-
                     //De Metros a centimetros, milimetros, kilometros, pulgadas, pies, yardas
-                    String unidad = (JOptionPane.showInputDialog(null, "seleccione una opcion de conversion", "Longitudes",
+                    Object unidad = (JOptionPane.showInputDialog(null, "seleccione una opcion de conversion", "Longitudes",
                             JOptionPane.PLAIN_MESSAGE, null, new Object[]{"De Metros a Centimetros", "De Metros a Milimetros", "De Metros a Kilometros",
                                     "De Metros a Pulgadas", "De Metros a Pies", "De Metros a Yardas", "De Centimetros a Metros", "De Milimetros a Metros",
-                                    "De Kilometros a Metros", "De Pulgadas a Metros", "De Pies a Metros", "De Yardas a Metros"}, "De Metros a Centimetros")).toString();
-
-                    JOptionPane.showMessageDialog(null, operacionLongitud.convertirLongitud(medidad, unidad));
-
-                    if (JOptionPane.OK_OPTION != JOptionPane.showConfirmDialog(null,"¿Deseas realizar otra conversión?")) {
+                                    "De Kilometros a Metros", "De Pulgadas a Metros", "De Pies a Metros", "De Yardas a Metros"}, "De Metros a Centimetros"));
+                    if (unidad == null) {
                         JOptionPane.showMessageDialog(null, "Programa terminado");
                         break;
+                    } else {
+                        JOptionPane.showMessageDialog(null, operacionLongitud.convertirLongitud(medidad, unidad.toString()));
+                        int opc = JOptionPane.showConfirmDialog(null, "¿Deseas realizar otra conversión?");
+                        if (opc != 0) {
+                            JOptionPane.showMessageDialog(null, "Programa terminado");
+                            break;
+                        }
                     }
                 } else {
                     JOptionPane.showMessageDialog(null, "Valor inválido");
                 }
 
             }
-
         }
 
     }
@@ -70,5 +83,4 @@ public class Main {
             return false;
         }
     }
-
 }
